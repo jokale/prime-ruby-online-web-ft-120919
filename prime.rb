815bf31968c8ba463c16c63d06fb793c4fd153c3
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 20:59:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,11 @@
-# Add  code here!
-def prime (number)
-   a = [2,3,5,11,13]
-  s = number.to_s
-  if a.include?(number) || s.end_with?("7")
+ a = [2,3,5,11,13]
+  s = num.to_s
+  if a.include?(num) || s.end_with?("7")
     return true
-  elsif s.end_with?("5") || num % 2 == 0 || number < 0
+  elsif s.end_with?("5") || num % 2 == 0 || num < 0
     return false
   else
     false
   end
 
-
-end 
+end
